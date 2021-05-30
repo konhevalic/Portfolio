c2701ef52de819778d94ea070c055f84c136484e
konhevalic <alan.patrick.konhevalic@gmail.com>
2021-05-30 16:48:30 -0300

    configurando a variavel para backend

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.REACT_APP_API_URL || 3000);
 
 const server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
